5abbd001733e052a076969637c114004e356b632
runtyalien <omgadkar@gmail.com>
2024-06-26 05:02:00 +0530

    Update route.js

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -10,8 +10,8 @@ export async function POST(req) {
   const s3client = new S3Client({
     region: 'ap-south-1',
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.OM_ACCESS_KEY,
+      secretAccessKey: process.env.OM_SECRET_ACCESS_KEY,
     },
   });
 
